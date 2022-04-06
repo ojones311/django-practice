acf9db8aed64000c5057299ed472f5e6557ea432
Owen Jones <owenthefirst93@gmail.com>
2022-04-05 22:55:01 -0400

    added views and linked app

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(req):
+    return HttpResponse('Welcome to our page')
+
+def january(req):
+    return HttpResponse('Read for 30 minutes a day')
+    
+def february(req):
+    return HttpResponse('Run two miles daily')
