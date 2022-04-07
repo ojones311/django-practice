8c3b37c8b0f13e5c5db52a2a931e662d1043c821
Owen Jones <owenthefirst93@gmail.com>
2022-04-06 23:04:05 -0400

    dynamic path and importing dict

--- challenges/views.py
+++ challenges/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .month import activity
 # Create your views here.
 
+#Create a month dict that maps a month to an activity
+#Pass the month dynamically which I say we would get it from the url
 
 def index(req):
     return HttpResponse('Welcome to our page')
 
-def january(req):
-    return HttpResponse('Read for 30 minutes a day')
-    
-def february(req):
-    return HttpResponse('Run two miles daily')
+def monthly_challenge(req, month):
+
+    return HttpResponse(f"In {month} my goal is to {activity[month]}")
